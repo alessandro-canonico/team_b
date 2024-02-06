c7453155b7c7306def1604f1950fdcfcea0426c3
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-02-06 16:43:42 +0100

    insert member properties

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const teamMembers = []
+const teamMembers = [
+  {
+    name: "Alessandro",
+    surname: "Canonico",
+    age: 27,
+    city: "Napoli",
+    hobby: "mixing vinyls",
+    favoriteFood: "Polpo alla luciana",
+    favoriteVideoGame: "",
+    favoriteFilm: "Non essere cattivo",
+    favoriteBook: "",
+    petName: "",
+  },
+];
