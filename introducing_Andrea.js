ba9c8bb371229578b3c18cc4b6df0b165a286a3a
Andrea.2308 <andreamail.2308@gmail.com>
2024-02-06 17:10:09 +0100

    add on local my introduction

---
+++ introducing_Andrea.js
@@ -0,0 +1,15 @@
+const team= {'Andre-23889':[
+        name= "Andrea",
+        surname= "Audisio",
+        age= 34,
+        city= "Cuneo",
+        hobby= "gaming",
+        favoriteFood= "lasagna",
+        favoriteVideoGame= "Minecraft",
+        favoriteFilm= "Star Wars, ep IV",
+        favoriteBook= "il ciclo delle Fondazioni",
+        petName= "Birba"
+    ]
+}
+
+console.log(team)
