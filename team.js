e4064290526512a2ae48eb0fabb3fd24ce194a90
Andrea.2308 <andreamail.2308@gmail.com>
2024-02-06 20:57:34 +0100

    added my introduction

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const teamMembers = []
+const teamMembers = [
+    {    
+        name: "Andrea",
+        surname: "Audisio",
+        age: 34,
+        city: "Cuneo",
+        hobby: ["serie tv", "Videogiochi", "fotografia"],
+        favoriteFood: "lasagne",
+        favoriteVideoGame: "Minecraft",
+        favoriteFilm: "Star Wars, canonico",
+        favoriteBook: "ciclo delle fondazioni",
+        petName: "Birba"
+    }
+]
