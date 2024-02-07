b55018e6185b0f6dbc2af931b72cc060fe1da6f6
Silas <silasampomah23@gmail.com>
2024-02-07 15:44:31 +0100

    new

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-const teamMembers = []
+const teamMembers = [{
+    
+    name : 'Silas',
+    surname : 'Bonsu',
+    age : 25,
+    city : 'Correggio',
+    hobby : 'atletica leggera',
+    favoriteFood :'pizza',
+    favoriteVideoGame : 'Horizon zero down',
+    favoriteFilm : 'Star wars',
+    favoriteBook : 'Il signore degli anelli',
+    petName : 'Bethoven',
+}]
