4c725db97594a9f36b8297df85e6785344a3384e
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-02-07 16:14:42 +0100

    add all members

--- team.js
+++ team.js
@@ -1,29 +1,28 @@
 const teamMembers = [
   {
-    
-    name : 'Silas',
-    surname : 'Bonsu',
-    age : 25,
-    city : 'Correggio',
-    hobby : 'atletica leggera',
-    favoriteFood :'pizza',
-    favoriteVideoGame : 'Horizon zero down',
-    favoriteFilm : 'Star wars',
-    favoriteBook : 'Il signore degli anelli',
-    petName : 'Bethoven',
-},
-    {    
-        name: "Andrea",
-        surname: "Audisio",
-        age: 34,
-        city: "Cuneo",
-        hobby: ["serie tv", "Videogiochi", "fotografia"],
-        favoriteFood: "lasagne",
-        favoriteVideoGame: "Minecraft",
-        favoriteFilm: "Star Wars, canonico",
-        favoriteBook: "ciclo delle fondazioni",
-        petName: "Birba"
-    },
+    name: "Silas",
+    surname: "Bonsu",
+    age: 25,
+    city: "Correggio",
+    hobby: "atletica leggera",
+    favoriteFood: "pizza",
+    favoriteVideoGame: "Horizon zero down",
+    favoriteFilm: "Star wars",
+    favoriteBook: "Il signore degli anelli",
+    petName: "Bethoven",
+  },
+  {
+    name: "Andrea",
+    surname: "Audisio",
+    age: 34,
+    city: "Cuneo",
+    hobby: ["serie tv", "Videogiochi", "fotografia"],
+    favoriteFood: "lasagne",
+    favoriteVideoGame: "Minecraft",
+    favoriteFilm: "Star Wars, canonico",
+    favoriteBook: "ciclo delle fondazioni",
+    petName: "Birba",
+  },
 
   {
     name: "Alessandro",
@@ -38,17 +37,16 @@ const teamMembers = [
     petName: "",
   },
 
-    {
-        name: "Ilyas",
-        surname: "Macaluso",
-        age: 26,
-        city: "Palermo",
-        hobby: ["Anime", "Manga", "Videogiochi"],
-        favoriteFood: "Hamburger americano",
-        favoriteVideoGame: "Guild Wars 2",
-        favoriteFilm: "Il signore degli anelli",
-        favoriteBook: "Dalla terra alla luna",
-        petName: ["Milo", "Luppolo"]
-    },
+  {
+    name: "Ilyas",
+    surname: "Macaluso",
+    age: 26,
+    city: "Palermo",
+    hobby: ["Anime", "Manga", "Videogiochi"],
+    favoriteFood: "Hamburger americano",
+    favoriteVideoGame: "Guild Wars 2",
+    favoriteFilm: "Il signore degli anelli",
+    favoriteBook: "Dalla terra alla luna",
+    petName: ["Milo", "Luppolo"],
+  },
 ];
-
