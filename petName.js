dc631b551be9958ee8b4a1f1b2fc529a24273cfb
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-02-08 17:39:53 +0100

    modify feature petName

--- petName.js
+++ petName.js
@@ -1,57 +1,58 @@
 const teamMembers = [
-    {
-      name: "Silas",
-      surname: "Bonsu",
-      age: 25,
-      city: "Correggio",
-      hobby: "atletica leggera",
-      favoriteFood: "pizza",
-      favoriteVideoGame: "Horizon zero down",
-      favoriteFilm: "Star wars",
-      favoriteBook: "Il signore degli anelli",
-      petName: "Bethoven",
-    },
-    {
-      name: "Andrea",
-      surname: "Audisio",
-      age: 34,
-      city: "Cuneo",
-      hobby: ["serie tv", "Videogiochi", "fotografia"],
-      favoriteFood: "lasagne",
-      favoriteVideoGame: "Minecraft",
-      favoriteFilm: "Star Wars, canonico",
-      favoriteBook: "ciclo delle fondazioni",
-      petName: "Birba",
-    },
-  
-    {
-      name: "Alessandro",
-      surname: "Canonico",
-      age: 27,
-      city: "Napoli",
-      hobby: "mixing vinyls",
-      favoriteFood: "Polpo alla luciana",
-      favoriteVideoGame: "",
-      favoriteFilm: "Non essere cattivo",
-      favoriteBook: "",
-      petName: "",
-    },
-  
-    {
-      name: "Ilyas",
-      surname: "Macaluso",
-      age: 26,
-      city: "Palermo",
-      hobby: ["Anime", "Manga", "Videogiochi"],
-      favoriteFood: "Hamburger americano",
-      favoriteVideoGame: "Guild Wars 2",
-      favoriteFilm: "Il signore degli anelli",
-      favoriteBook: "Dalla terra alla luna",
-      petName: ["Milo", "Luppolo"],
-    },
-  ];
+  {
+    name: "Silas",
+    surname: "Bonsu",
+    age: 25,
+    city: "Correggio",
+    hobby: "atletica leggera",
+    favoriteFood: "pizza",
+    favoriteVideoGame: "Horizon zero down",
+    favoriteFilm: "Star wars",
+    favoriteBook: "Il signore degli anelli",
+    petName: "Bethoven",
+  },
+  {
+    name: "Andrea",
+    surname: "Audisio",
+    age: 34,
+    city: "Cuneo",
+    hobby: ["serie tv", "Videogiochi", "fotografia"],
+    favoriteFood: "lasagne",
+    favoriteVideoGame: "Minecraft",
+    favoriteFilm: "Star Wars, canonico",
+    favoriteBook: "ciclo delle fondazioni",
+    petName: "Birba",
+  },
 
+  {
+    name: "Alessandro",
+    surname: "Canonico",
+    age: 27,
+    city: "Napoli",
+    hobby: "mixing vinyls",
+    favoriteFood: "Polpo alla luciana",
+    favoriteVideoGame: "",
+    favoriteFilm: "Non essere cattivo",
+    favoriteBook: "",
+    petName: "",
+  },
 
-const membersWithPet = teamMembers.filter(pet => pet.petName != "")
+  {
+    name: "Ilyas",
+    surname: "Macaluso",
+    age: 26,
+    city: "Palermo",
+    hobby: ["Anime", "Manga", "Videogiochi"],
+    favoriteFood: "Hamburger americano",
+    favoriteVideoGame: "Guild Wars 2",
+    favoriteFilm: "Il signore degli anelli",
+    favoriteBook: "Dalla terra alla luna",
+    petName: ["Milo", "Luppolo"],
+  },
+];
+
+const membersWithPet = teamMembers
+  .filter((members) => members.petName != "")
+  .map((members) => members.name + `: ` + members.petName);
 
 console.log(membersWithPet);
