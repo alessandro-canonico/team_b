48583c44f3cd3c8757ae9f169360a3be07f02f82
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-02-15 18:07:53 +0100

    create files and functions drafts

---
+++ completeTask.js
@@ -0,0 +1,18 @@
+let tasks = []
+
+function completeTask (index, callback) {
+    console.log(`Completing the task...`);
+    if (index < 0 || index >= tasks.length) {
+        callback(new Error(`Invalid task index`), null)
+    } else { tasks.splice(index, 0)
+        callback(null, `Task completed successfully`)
+    }
+}
+
+function handlingTasksError (error, data) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+}
