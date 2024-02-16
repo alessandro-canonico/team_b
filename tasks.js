e7e539849570f68ddf62598d52f545f9dcb8960f
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-02-16 10:57:00 +0100

    add optimised version of tasks.js

--- tasks.js
+++ tasks.js
@@ -1,47 +1,56 @@
-let tasks = [`Backup OS`, `Push tasks.js,`];
+let tasks = [`Backup OS`, `Push tasks.js`, `Check Google Calendar`];
 
 //function1: addTask
-function addTask(task, callback){
-    if (typeof task === "string" && task !== ""){
-        setTimeout(() => {
-            tasks.push(task)
-            callback(null, `Task added successfully`)
-        }, 1000)
+function addTask(task, callback) {
+  console.log("Adding task...")
+  setTimeout(() => {
+    if (typeof task === "string" && task !== "") {
+      tasks.push(task);
+      callback(null, `Task added successfully`)
     } else {
-        callback(new Error ("Task must be a non empy string"), null)
+      callback(new Error("Task must be a non empy string"), null)
     }
+  }, 1000)
 }
 
-addTask("Push task.js to GitHub repo", screenMessage)
+addTask(10, screenMessage);
 
 //function2: complete task
-function completeTask(index, callback){
-    if (index < tasks.length && index >= 0){
-        setTimeout(() => {
-            tasks.splice(index, 1)
-            callback(null, `Task completed successfully`)
-        }, 2000)
+function completeTask(index, callback) {
+  setTimeout(() => {
+    console.log("Removing completed task...")
+  }, 2000);
+  setTimeout(() => {
+    if (index < tasks.length && index >= 0) {
+      tasks.splice(index, 1)
+      callback(null, `Task removed successfully`)
     } else {
-        callback(new Error("Invalid task index"))
+      callback(new Error("Invalid task index"))
     }
+  }, 3000)
 }
 
-completeTask(0, screenMessage)
+completeTask(0, screenMessage);
 
 //callback function for function 1 & 2
-function screenMessage (error, message){
-    if (message){
-        console.log(message);
-    } else {
-        console.log(error);
-    }
+function screenMessage(error, message) {
+  if (message) {
+    console.log(message)
+  } else {
+    console.log(error)
+  }
 }
 
 //function3: list all tasks on console
 function listTasks(tasksArray) {
+  console.log("Remaining tasks:")
   tasksArray.forEach((task) => {
-    console.log(task);
-  });
+    // setTimeout(() => {
+    console.log(`- ${task}`)
+    // }, 5000)
+  })
 }
 
-listTasks(tasks);
+setTimeout(() => {
+  listTasks(tasks)
+}, 4000)
