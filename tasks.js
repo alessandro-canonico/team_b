a0474490c23c877ad60130b15c2d528ee40c182a
alessandro-canonico <alessandrocanonico96@gmail.com>
2024-02-19 16:32:42 +0100

    optimize file tasks

--- tasks.js
+++ tasks.js
@@ -2,15 +2,15 @@ let tasks = [`Backup OS`, `Push tasks.js`, `Check Google Calendar`];
 
 //function1: addTask
 function addTask(task, callback) {
-  console.log("Adding task...")
+  console.log("Adding task...");
   setTimeout(() => {
     if (typeof task === "string" && task !== "") {
       tasks.push(task);
-      callback(null, `Task added successfully`)
+      callback(null, `Task added successfully`);
     } else {
-      callback(new Error("Task must be a non empy string"), null)
+      callback(new Error("Task must be a non empy string"), null);
     }
-  }, 1000)
+  }, 1000);
 }
 
 addTask(10, screenMessage);
@@ -18,16 +18,16 @@ addTask(10, screenMessage);
 //function2: complete task
 function completeTask(index, callback) {
   setTimeout(() => {
-    console.log("Removing completed task...")
+    console.log("Removing completed task...");
   }, 2000);
   setTimeout(() => {
     if (index < tasks.length && index >= 0) {
-      tasks.splice(index, 1)
-      callback(null, `Task removed successfully`)
+      tasks.splice(index, 1);
+      callback(null, `Task removed successfully`);
     } else {
-      callback(new Error("Invalid task index"))
+      callback(new Error("Invalid task index"), null);
     }
-  }, 3000)
+  }, 3000);
 }
 
 completeTask(0, screenMessage);
@@ -35,22 +35,20 @@ completeTask(0, screenMessage);
 //callback function for function 1 & 2
 function screenMessage(error, message) {
   if (message) {
-    console.log(message)
+    console.log(message);
   } else {
-    console.log(error)
+    console.log(error);
   }
 }
 
 //function3: list all tasks on console
 function listTasks(tasksArray) {
-  console.log("Remaining tasks:")
+  console.log("Remaining tasks:");
   tasksArray.forEach((task) => {
-    // setTimeout(() => {
-    console.log(`- ${task}`)
-    // }, 5000)
-  })
+    console.log(`- ${task}`);
+  });
 }
 
 setTimeout(() => {
-  listTasks(tasks)
-}, 4000)
+  listTasks(tasks);
+}, 4000);
